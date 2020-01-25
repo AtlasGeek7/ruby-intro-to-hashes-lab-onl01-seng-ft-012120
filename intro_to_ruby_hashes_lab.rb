3641fd157500c3bf45d8033f4aad1cb66aab7317
Khalid T <khalid.tissire@gmail.com>
2020-01-25 05:48:39 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,3 +1,11 @@
+def new_hash
+  {}
+end
+
+def actor
+  actor = {name: "Dwayne The Rock Johnson"}
+end
+
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
